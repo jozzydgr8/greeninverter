1be1be568a7cddc8e16bc4a251ed5e04ba43518a
jozzydgr8 <jozzydgreat1@gmail.com>
2025-06-30 04:27:31 -0700

    updated ul

--- src/pages/component/Services.tsx
+++ src/pages/component/Services.tsx
@@ -5,42 +5,54 @@ export const Services = ()=>{
         <section>
             <div className="container-fluid">
                 <div className="servicegrid">
-                    {
-                        service.map((service,index)=>(
-                            <div className="servicergridcontent" key={index}>
-                                <div
-                                    style={{
-                                    backgroundImage: `url(${service.image})`,
-                                    height: "200px",
-                                    backgroundSize: "cover",
-                                    backgroundPosition: "center center",
-                                    backgroundRepeat: "no-repeat",
-                                    }}
-                                ></div>
-                                <div
-                                    style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                    flex: 1,
-                                    padding: "15px",
-                                    }}
-                                >
-                                <h3>{service.title}</h3>
-                                <ul>
-                                    {
-                                    service.service.map((service, index)=>(
-                                        <li>
-                                            {service}
-                                        </li>
-                                    ))
-                                }
-                                </ul>
-                                
-                                </div>
-                                
-                            </div>
-                        ))
-                    }
+                   {
+  service.map((serviceItem, index) => (
+    <div className="servicergridcontent" key={index}>
+      <div
+        style={{
+          backgroundImage: `url(${serviceItem.image})`,
+          height: "200px",
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+          borderRadius: "8px",
+        }}
+      ></div>
+      
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        padding: "15px",
+        gap: "10px",
+      }}>
+        <h3>{serviceItem.title}</h3>
+
+        <div style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "8px",
+        }}>
+          {
+            serviceItem.service.map((item, i) => (
+              <div key={i} style={{
+                backgroundColor: "#e0f7e9", // light green tint for energy theme
+                color: "#0d684f",
+                padding: "6px 12px",
+                borderRadius: "20px",
+                fontSize: "14px",
+                fontWeight: "500",
+              }}>
+                {item}
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  ))
+}
+
                 </div>
             </div>
         </section>
